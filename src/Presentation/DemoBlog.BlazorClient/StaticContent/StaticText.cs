16117ae4087b3b78b1d06ae7ddd76b0b22830186
Sylvester Knudsen <sylvesterknudsen@gmail.com>
2023-02-19 21:47:58 +0000

    so many things

---
+++ src/Presentation/DemoBlog.BlazorClient/StaticContent/StaticText.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoBlog.BlazorClient.StaticContent
+{
+    public class StaticText
+    {
+        public class FrontPageTextContent
+        {
+            internal const string WELCOME_TITLE = "Welcome to the Blog";
+            internal const string WELCOME_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus feugiat nisi orci, sit amet blandit elit maximus dapibus. Suspendisse ut dictum turpis. Nunc sem odio, vestibulum eu pharetra sed, finibus in mi. Proin non mollis eros, consequat eleifend est. Pellentesque sit amet mi convallis, ullamcorper ex in, sollicitudin orci. Maecenas vitae ligula vel justo ultrices ullamcorper a sit amet sapien. Sed a ligula sed magna sagittis bibendum.";
+        }
+    }
+}
---
+++ src/Presentation/DemoBlog.BlazorClient/Themes/BlogTheme.cs
@@ -0,0 +1,18 @@
+﻿using MudBlazor;
+using MudBlazor.Utilities;
+
+namespace DemoBlog.BlazorClient.Themes
+{
+    public class BlogTheme : MudTheme
+    {
+        public BlogTheme()
+        {
+            this.Palette = new Palette
+            {
+                Primary = new MudColor("#8B5000"),
+                Secondary = new MudColor("#785900"),
+                Tertiary = new MudColor("#735C00"),
+            };
+        }
+    }
+}
