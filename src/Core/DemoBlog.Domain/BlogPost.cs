d267ea2026d95bc6c889214b62e81c2883f5ac0b
Sylvester Knudsen <sylvesterknudsen@gmail.com>
2023-03-03 16:21:38 +0000

    post editor (#14)

---
+++ src/Core/DemoBlog.Domain/BlogPost.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoBlog.Domain
+{
+    public class BlogPost
+    {
+        public int MyProperty { get; set; }
+    }
+}
